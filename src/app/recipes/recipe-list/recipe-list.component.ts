84facbfbd2aa97307bfa46d190de7c5cd696d1a1
Kwabena Agyeman <kagyeman@ymail.com>
2023-07-24 21:47:21 +0000

    set up styling for recipe list item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    {
+      name: "Kwabena's recipe",
+      description: ' Dummy recipe',
+      imagePath:
+        'https://eugenesdiner.com/wp-content/uploads/2023/01/NewPunjabClub_Overhead2.jpg',
+    },
+    {
+      name: "Kwabena's recipe",
+      description: ' Dummy recipe',
+      imagePath:
+        'https://eugenesdiner.com/wp-content/uploads/2023/01/NewPunjabClub_Overhead2.jpg',
+    },
+  ];
 }
