009dfcc1c87da70bcc73d26e86a7224d5e67f62a
Kwabena Agyeman <kagyeman@ymail.com>
2023-07-27 16:38:04 +0000

    Set up ingredient form with template driven approach

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,10 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { ShoppingListService } from 'src/app/services/shopping-list.service';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
@@ -8,12 +14,15 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
   styleUrls: ['./shopping-list-edit.component.css'],
 })
 export class ShoppingListEditComponent {
+  @ViewChild('form') IngredientForm?: HTMLFormElement;
+
   constructor(private shopListService: ShoppingListService) {}
 
   name: string = '';
-  amount: number = 0;
+  amount?: number;
 
   onAddItem(item: Ingredient) {
     this.shopListService.addIngredient(item);
+    this.IngredientForm?.reset();
   }
 }
