62dc4feedf17d2018e20db90c28a554f5d74ad51
Kwabena Agyeman <kagyeman@ymail.com>
2023-07-29 18:48:14 +0000

    Added logic to hide router links based on auth state

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,38 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { DataStorageService } from '../services/data-storage.service';
+import { AuthService } from '../services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   isCollapsed = false;
+  isAuthenticated = false;
+  private userSubscription!: Subscription;
 
-  constructor(private dataService: DataStorageService) {}
+  constructor(
+    private dataService: DataStorageService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.userSubscription = this.authService.user.subscribe((user) => {
+      this.isAuthenticated = !!user;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
 
   onSaveData() {
     this.dataService.storeRecipes();
@@ -18,4 +41,6 @@ export class HeaderComponent {
   onFetchData() {
     this.dataService.fetchRecipes();
   }
+
+  onSignOut() {}
 }
